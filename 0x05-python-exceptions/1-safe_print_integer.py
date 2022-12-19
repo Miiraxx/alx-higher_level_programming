025d5c3f9607dce2177b460fa41061d5b1f684ba
Miracle Salami <salami.miracle.o@gmail.comcdsalami.miracle.o@gmailcd /alx-pre_course//alx-pre_course# git config --global user.name miracle>
2022-12-19 12:13:23 -0800

    task 1-6

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,12 +3,12 @@ def safe_print_integer(value):
     """Print an integer with "{:d}".format().
     Args:
         value (int): The integer to print.
-        Returns:
+    Returns:
             If a TypeError or ValueError occurs - False.
             Otherwise - True.
-        """
-        try:
-            print("{:d}".format(value))
+    """
+    try:
+        print("{:d}".format(value))
             return (True)
-        except (TypeError, ValueError):
+    except (TypeError, ValueError):
             return (False)
