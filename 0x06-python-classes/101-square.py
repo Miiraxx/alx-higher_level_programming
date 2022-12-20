031edd24cf4b51c5ae1444adb34b2fe6e8e58cbd
Miracle Salami <salami.miracle.o@gmail.comcdsalami.miracle.o@gmailcd /alx-pre_course//alx-pre_course# git config --global user.name miracle>
2022-12-20 06:24:43 -0800

    advanced

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -5,14 +5,16 @@
 
 class Square:
     """Represent a square."""
+
     def __init__(self, size=0, position=(0, 0)):
-        """Initialize a new square.
-        Args:
-            size (int): The size of the new square.
-            position (int, int): The position of the new square.
-        """
-        self.size = size
-        self.position = position
+    """Initialize a new square.
+    
+    Args:
+        size (int): The size of the new square.
+        position (int, int): The position of the new square.
+    """
+    self.size = size
+    self.position = position
 
     @property
     def size(self):
@@ -28,7 +30,7 @@ class Square:
         self.__size = value
 
     @property
-    def position(self):
+    def position(self)
         """Get/set the current position of the square."""
         return (self.__position)
 
@@ -50,9 +52,20 @@ class Square:
         if self.__size == 0:
             print("")
             return
-    
+
         [print("") for i in range(0, self.__position[1])]
         for i in range(0, self.__size):
             [print(" ", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for k in range(0, self.__size)]
             print("")
+
+    def __str__(self):
+        """Define the print() representation of a Square."""
+        if self.__size != 0:
+            [print("") for i in range(0, self.__position[1])]
+            for i in range(0, self.__size):
+                [print(" ", end="") for j in range(0, self.__position[0])]
+                [print("#", end="") for k in range(0, self.__size)]
+                if i != self.__size - 1:
+                    print("")
+            return ("")
