759f0867472e22e58d803a5f3176cca40382c26c
Miracle Salami <salami.miracle.o@gmail.comcdsalami.miracle.o@gmailcd /alx-pre_course//alx-pre_course# git config --global user.name miracle>
2023-01-11 14:09:24 -0800

    Task done

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     import sys
 
     size = 0
-    tatus_codes = {}
+    status_codes = {}
     valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
     count = 0
 
